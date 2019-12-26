0638a2d4a0855f2529ca6d78c727f5de8ea3f73b
chengzi666 <1125200963@qq.com>
2019-12-26 18:05:05 +0800

    leyou

--- leyou-item/leyou-item-service/src/main/java/com/leyou/LeyouItemApplication.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/LeyouItemApplication.java
@@ -1,5 +1,6 @@
 package com.leyou;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
@@ -9,9 +10,13 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author:chenyun
  * @Date:2019/12/23 17:09
  */
-//springboot启动类，其中包含了@EnableAutoConfiguration  @SpringBootConfiguration  @ComponentScan
+
+// springboot启动类， 其中包含了@EnableAutoConfiguration  @SpringBootConfiguration  @ComponentScan
 @SpringBootApplication
 //eureka的客户端启动注解
 @EnableDiscoveryClient
 public class LeyouItemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouItemApplication.class,args);
+    }
 }
